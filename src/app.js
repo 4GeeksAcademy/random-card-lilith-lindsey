6680ea73fa307d7f413d7e9ee88ec775120ba513
Lilith Lindsey <lilithx1313@gmail.com>
2025-08-11 22:33:13 +0000

    Finished up the project with CSS styling to give it the appearance of an actual card

--- src/app.js
+++ src/app.js
@@ -36,37 +36,5 @@ window.onload = function() {
     location.reload();
   });
 };
-  // const numDiv = document.querySelector("#face");
-
-  // const numArr = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-  
-  // const randomDec = Math.random();
-  // const randomLongNum = randomDec * numArr.length;
-  // const randomNumNoDec = Math.floor(randomLongNum);
-
-  // numDiv.innerHTML = numArr[randomNumNoDec];
-
-  // const suitDiv = document.querySelector("#suit");
-
-  // const suitArr = ["♦", "♥", "♠", "♣"];
-
-  // const randomDecSuit = Math.random();
-  // const randomLongSuit = randomDecSuit * suitArr.length;
-  // const randomSuitNoDec = Math.floor(randomLongSuit)
-  //     if(suitArr[randomSuitNoDec] == "♦" || suitArr[randomSuitNoDec] == "♥"){
-  //         suitDiv.
-  //     }
-
-  // suitDiv.innerHTML = suitArr[randomSuitNoDec];
-  
-
-  // const refreshButton = document.querySelector("#refresh");
-
-  // refreshButton.addEventListener("click", () => {
-  //     location.reload();
-  //   } 
-  // );
-  
-  // }
 
 
